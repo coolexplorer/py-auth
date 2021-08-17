57e20e52c228e9e10f80ed6039afe94e39e87c50
seunkim <seunkim@ea.com>
2021-08-16 23:51:23 -0700

    Changed unit test

--- test/test_root.py
+++ test/test_root.py
@@ -7,10 +7,12 @@ import json
 from httpx import AsyncClient
 
 from app.main import app
+import app.schemas.base_response as base_response_schema
+
 
 @pytest.mark.asyncio
 async def test_ping():
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get("/v1/ping")
     assert response.status_code == 200
-    assert json.dumps(eval(response.text)) == json.dumps({'result': 'pong'})
+    assert json.dumps(eval(response.text)) == json.dumps(base_response_schema.BaseResponse(result=True))
